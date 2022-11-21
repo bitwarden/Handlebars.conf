f15ad1e6a3ce6efa03e6921adf8f3917778b2f03
Kyle Spearrin <kyle.spearrin@gmail.com>
2022-11-21 10:32:17 -0500

    basic logic

---
+++ src/Handlebars.conf/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace Handlebars.conf;
+
+internal class Config
+{
+    public string Source { get; set; }
+    public string Destination { get; set; }
+}
--- src/Handlebars.conf/Program.cs
+++ src/Handlebars.conf/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+
+namespace Handlebars.conf;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var configFileOption = new Option<FileInfo>(
+            name: "--config",
+            description: "The file to read and display on the console.");
+
+        var rootCommand = new RootCommand("Handlebars templates for config files.");
+        rootCommand.AddOption(configFileOption);
+
+        rootCommand.SetHandler(async (configFile) =>
+        {
+            var configArray = await ReadConfigFileAsync(configFile);
+            foreach (var config in configArray)
+            {
+                var source = await File.ReadAllTextAsync(config.Source);
+                var sourceTemplate = HandlebarsDotNet.Handlebars.Compile(source);
+                var result = sourceTemplate(new Dictionary<string, object>
+                {
+                    { "env", Environment.GetEnvironmentVariables() }
+                });
+                await File.WriteAllTextAsync(config.Destination, result);
+            }
+        }, configFileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static async Task<Config[]> ReadConfigFileAsync(FileInfo file)
+    {
+        var yaml = await File.ReadAllTextAsync(file.FullName);
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+        return deserializer.Deserialize<Config[]>(yaml);
+    }
+}
